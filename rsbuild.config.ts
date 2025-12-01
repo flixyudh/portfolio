441e11eb54bc93adee97dc037769d66440086271
Flix <flixy121@gmail.com>
2025-12-01 11:23:49 +0700

    coba

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,7 +4,4 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
-  output:{
-    assetPrefix: '/portfolio/',
-  }
 });
